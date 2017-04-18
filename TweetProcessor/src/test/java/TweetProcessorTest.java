ba92034b7214a08e411a99f9546420c1767b7a24
Robert de Haan <robertdehaan@gmail.com>
2017-04-18 10:06:42 +0200

    (TweetProcessor) Added test class.

--- TweetProcessor/src/test/java/TweetProcessorTest.java
+++ TweetProcessor/src/test/java/TweetProcessorTest.java
@@ -1,37 +1,14 @@
-import com.google.gson.Gson;
 import data.PredictedLocation;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 
 public class TweetProcessorTest {
 	@Test
-	public void testJsonOutput() throws Exception {
-		List<PredictedLocation> predictedLocationList = new ArrayList<>();
-
-		PredictedLocation predictedLocation = new PredictedLocation("Schipborg", "Drenthe");
-		PredictedLocation anotherPredictedLocation = new PredictedLocation("Groningen", "Groningen");
-
-		predictedLocationList.add(predictedLocation);
-		predictedLocationList.add(anotherPredictedLocation);
-
-		Gson gson = new Gson();
-		System.out.println(gson.toJson(predictedLocationList));
-		assertNotNull(gson);
-	}
-
-	@Test
-	public void testGetResourceFile() throws Exception {
-		try (InputStream in = TweetProcessor.class.getResourceAsStream("/places.json")) {
-			String result = IOUtils.toString(in, StandardCharsets.UTF_8);
-			System.out.println(result);
-		}
+	public void testGuessLocation() throws Exception {
+		PredictedLocation predictedLocation = TweetProcessor.guessLocation("Amsterdam, The Netherlands");
+		PredictedLocation bestMatch = new PredictedLocation("Amsterdam", "Noord-Holland");
+		assertEquals(predictedLocation, bestMatch);
 	}
 }
